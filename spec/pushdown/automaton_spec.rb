0a1bdef5e82ad97240863b9914a2cca1289aa668
Michael Granger <ged@FaerieMUD.org>
2019-12-24 12:07:04 -0800

    Initial commit.

---
+++ spec/pushdown/automaton_spec.rb
@@ -0,0 +1,14 @@
+# -*- ruby -*-
+# frozen_string_literal: true
+
+require_relative '../spec_helper'
+
+require 'rspec'
+require 'pushdown/automaton'
+
+RSpec.describe Pushdown::Automaton do
+
+	
+
+end
+
